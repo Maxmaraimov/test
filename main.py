da2a9643d02af5431f0cfa783ea80752b4588512
JASUR <jasur@mail.ru>
2022-01-15 14:35:27 +0500

    birinchi save

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-print('jasur')
-def file():
-    return 'ghj'
+# https://www.youtube.com/watch?v=ROBQW21WaLU&t=84s
+
+# git init - git file ni yaratadi
+
+
+
+# git commit -m 'save name' - gitni sohraniniya qiladi
+
+# git log -p -2 ohirgi ikkita git commit
+
+
+
+# git reset HASH --hard - hash bo'yicha orqaga qaytadi
+
+# git branch - qaysi vitkada ekanligini ko'rsatadi
+
+# git branch NAME
+
+
+
+
+
+
+
+
+# git config --global user.name "Maxmaraimiv" - bu bir marta yoziladi nastroyka qilib olish uchun.bu faqat ustanovka bo'laganda.
+
+# git config --global user.email "jasurbek19971997@mail.ru"
+
+# git init - git file ni yaratadi
+
+#  ls -lh - bu skritiy file ni ko'rish.
+
+# git status - kim kuztilyapdi ko'rsatiladi
+
+# git add . - hamma file ni kuzatasan
